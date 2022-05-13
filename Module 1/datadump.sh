63ddb72f0a9d60fdf41ebb294bd2781b8b816780
fadialsalti <81257286+fadialsalti@users.noreply.github.com>
2022-05-13 13:19:47 +0200

    Add files via upload

---
+++ Module 1/datadump.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+MYSQL_HOST='127.0.0.1'
+MYSQL_PORT='3306'
+MYSQL_USER='root'
+MYSQL_PASSWORD='ODE4NC1lbmdmYWRp'
+DATABASE_NAME='sales'
+
+echo "Backup started for database - ${DATABASE_NAME}"
+
+mysqldump -h 127.0.0.1 -u root -pODE4NC1lbmdmYWRp --databases sales 
+mysqldump -h ${MYSQL_HOST} \
+-P ${MYSQL_PORT} \
+-u ${MYSQL_USER} \
+-p${MYSQL_PASSWORD} \
+${DATABASE_NAME} > salesdata.sql
+
+if [ $? -eq 0 ]; then
+echo "Database backup successfully completed"
+else
+echo "Error found during backup"
+exit 1
+fi
